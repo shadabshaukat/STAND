fbe20ab1cb15e3555b5985855a35c003a1fbf899
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2023-03-01 23:19:25 +1100

    Update oracle.sql

--- oracle.sql
+++ oracle.sql
@@ -1,7 +1,7 @@
 -- Oracle Database --
 
 CREATE TABLE orders (
-  order_id NUMBER(38) PRIMARY KEY,
+  order_id NUMBER(38) GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   customer_id NUMBER(38),
   product_id NUMBER(38),
   product_description VARCHAR2(500),
@@ -10,44 +10,30 @@ CREATE TABLE orders (
   order_misc_notes VARCHAR2(500)
 );
 
-
--- PL/SQL Procedure to INSERT 'n' random Records to 'orders' Table -- 
+-- Create Procedure to insert 'n' random records --
+-- We're generating random data for the 'orders' table and storing it in a collection. We're then using the GENERATED ALWAYS AS IDENTITY column for the 'order_id' field to automatically generate unique 'order_id' values for each new row inserted into the table.
+-- We're also using the BULK COLLECT feature to insert the entire collection of 'orders' records into the database at once, which can be faster than inserting each row one at a time.
+-- With these modifications, the procedure should be able to generate and insert a large number of random orders into the 'orders' table quickly and efficiently.
 
 CREATE OR REPLACE PROCEDURE add_random_orders(n IN NUMBER)
 IS
-  i NUMBER := 1;
-  v_order_id NUMBER;
-  CURSOR c_orders IS
-    SELECT order_id FROM orders;
+  TYPE t_orders IS TABLE OF orders%ROWTYPE;
+  v_orders t_orders := t_orders();
 BEGIN
-  WHILE i <= n LOOP
-    v_order_id := i;
-    -- Check if record with same order_id exists
-    FOR o IN c_orders LOOP
-      IF v_order_id = o.order_id THEN
-        v_order_id := v_order_id + 1;
-      END IF;
-    END LOOP;
-    -- Insert new record with updated order_id
-    INSERT INTO orders (
-      order_id, 
-      customer_id, 
-      product_id, 
-      product_description, 
-      order_delivery_address, 
-      order_date_taken, 
-      order_misc_notes
-    ) VALUES (
-      v_order_id,
-      FLOOR(DBMS_RANDOM.VALUE(1, 100)),
-      FLOOR(DBMS_RANDOM.VALUE(1, 50)),
-      'Product ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1),
-      'Address ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1),
-      TRUNC(SYSDATE) - FLOOR(DBMS_RANDOM.VALUE(1, 30)),
-      'Note ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1)
-    );
-    i := i + 1;
+  -- Generate random orders and add to collection
+  FOR i IN 1..n LOOP
+    v_orders.EXTEND;
+    v_orders(i).customer_id := FLOOR(DBMS_RANDOM.VALUE(1, 100));
+    v_orders(i).product_id := FLOOR(DBMS_RANDOM.VALUE(1, 50));
+    v_orders(i).product_description := 'Product ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1);
+    v_orders(i).order_delivery_address := 'Address ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1);
+    v_orders(i).order_date_taken := TRUNC(SYSDATE) - FLOOR(DBMS_RANDOM.VALUE(1, 30));
+    v_orders(i).order_misc_notes := 'Note ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1);
   END LOOP;
+  -- Insert collection into database using BULK COLLECT
+  FORALL i IN 1..v_orders.COUNT
+    INSERT INTO orders (customer_id, product_id, product_description, order_delivery_address, order_date_taken, order_misc_notes)
+    VALUES (v_orders(i).customer_id, v_orders(i).product_id, v_orders(i).product_description, v_orders(i).order_delivery_address, v_orders(i).order_date_taken, v_orders(i).order_misc_notes);
   COMMIT;
   DBMS_OUTPUT.PUT_LINE(n || ' random orders added.');
 EXCEPTION
@@ -57,11 +43,17 @@ EXCEPTION
 END;
 /
 
-
--- Generate 1,000 Random Records --
+-- Execute Procedure to Generate
 
 BEGIN
-  add_random_orders(1000);
+  add_random_orders(100000);
 END;
 
+-- Query Table --
 
+select * from  orders;
+
+-- Drop Objects After Testing --
+
+drop table orders;
+drop procedure add_random_orders;
