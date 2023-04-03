29cb616a74e0eb167ce1495903f34aa43603463f
Shadab Mohammad <39692236+shadabshaukat@users.noreply.github.com>
2023-04-03 19:30:03 +1000

    Update oracle11g.sql

--- oracle11g.sql
+++ oracle11g.sql
@@ -1,4 +1,5 @@
--- Oracle Database Version 11.2.0.4 Compatible --
+-- Oracle Database Version 11.2 Compatible --
+
 -- Create orders table
 CREATE TABLE orders (
   order_id NUMBER(38) PRIMARY KEY,
@@ -23,3 +24,45 @@ END;
 /
 
 
+-- Create Procedure to Generate 'n' Random Records --
+CREATE OR REPLACE PROCEDURE add_random_orders(n IN NUMBER)
+IS
+  TYPE t_orders IS TABLE OF orders%ROWTYPE;
+  v_orders t_orders := t_orders();
+BEGIN
+  -- Generate random orders and add to collection
+  FOR i IN 1..n LOOP
+    v_orders.EXTEND;
+    v_orders(i).customer_id := FLOOR(DBMS_RANDOM.VALUE(1, 100));
+    v_orders(i).product_id := FLOOR(DBMS_RANDOM.VALUE(1, 50));
+    v_orders(i).product_description := 'Product ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1);
+    v_orders(i).order_delivery_address := 'Address ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1);
+    v_orders(i).order_date_taken := TRUNC(SYSDATE) - FLOOR(DBMS_RANDOM.VALUE(1, 30));
+    v_orders(i).order_misc_notes := 'Note ' || (FLOOR(DBMS_RANDOM.VALUE(1, 10)) + 1);
+  END LOOP;
+  -- Insert collection into database using BULK COLLECT
+  FORALL i IN 1..v_orders.COUNT
+    INSERT INTO orders (customer_id, product_id, product_description, order_delivery_address, order_date_taken, order_misc_notes)
+    VALUES (v_orders(i).customer_id, v_orders(i).product_id, v_orders(i).product_description, v_orders(i).order_delivery_address, v_orders(i).order_date_taken, v_orders(i).order_misc_notes);
+  COMMIT;
+  DBMS_OUTPUT.PUT_LINE(n || ' random orders added.');
+EXCEPTION
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+    ROLLBACK;
+END;
+/
+
+
+
+-- Execute Procedure --
+BEGIN
+  add_random_orders(1000);
+END;
+/
+
+
+-- Cleanup --
+
+--drop table orders;
+--drop procedure add_random_orders;
